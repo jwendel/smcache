38dc1f5ed28b7c6a3ba1d8cd0f5f41476818e095
James Wendel <jmwendel@gmail.com>
2020-01-28 21:56:56 -0700

    Port over the cache impl from other project. Don't sanatize ProjectId. Add some more comments.

--- cache.go
+++ cache.go
@@ -1,17 +1,220 @@
 package gsmcache
 
-import "log"
+import (
+	"context"
+	"fmt"
+	"log"
+	"regexp"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1beta1"
+	"golang.org/x/crypto/acme/autocert"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1beta1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
 const debug = false
 
-// SMCache TODO
-type SMCache struct {
-	ProjectId    string
+// GSMCache represents a autocert cache that will store/retreive data from
+// GCP Secret Manager.
+type GSMCache struct {
+	// ProjectId is the GCP Project ID where the Secrets will be stored
+	ProjectId string
+	// SecretPrefix is a string that will be put before the secret name.
+	// This is useful for for IAM access control and for grouping secrets
+	// by application.
 	SecretPrefix string
+}
+
+// Get returns a certificate data for the specified key.
+// If there's no such key, Get returns ErrCacheMiss.
+func (smc *GSMCache) Get(ctx context.Context, key string) ([]byte, error) {
+	key = sanitize(key)
+
+	dlog("Get called for: [%v]", key)
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to setup client: %w", err)
+	}
+
+	svKey := fmt.Sprintf("projects/%s/secrets/%s%s/versions/latest", smc.ProjectId, smc.SecretPrefix, key)
+	dlog("GET svKey: %v", svKey)
+
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: svKey,
+	}
+	resp, err := client.AccessSecretVersion(ctx, req)
+
+	if st := status.Convert(err); st != nil {
+		if st.Code() == codes.NotFound {
+			return nil, autocert.ErrCacheMiss
+		}
+		return nil, err
+	}
+
+	dlog("GET: Got result: %+v", resp.GetName())
+	return resp.GetPayload().GetData(), nil
+}
+
+// Put stores the data in the cache under the specified key.
+// Underlying implementations may use any data storage format,
+// as long as the reverse operation, Get, results in the original data.
+func (smc *GSMCache) Put(ctx context.Context, key string, data []byte) error {
+	key = sanitize(key)
+
+	dlog("Put called for: [%v]", key)
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("Failed to setup client: %w", err)
+	}
+
+	// Get a List of SecretVersions that already exist in this secret.
+	// If we get NotFound, we know to create the secret.
+	// Otherwise we'll have a list of SecretVersions to delete once the rest is complete.
+	svi := client.ListSecretVersions(context.Background(), &secretmanagerpb.ListSecretVersionsRequest{
+		Parent: fmt.Sprintf("projects/%s/secrets/%s%s", smc.ProjectId, smc.SecretPrefix, key),
+		// Should only need to get a few to delete.  Also hopefully they are returned in most-recent-first order
+		PageSize: 10,
+	})
+
+	sv, err := svi.Next()
+	if st := status.Convert(err); st != nil {
+		if st.Code() == codes.NotFound {
+			// If the base Secret was NotFound, we attempt to create it
+			err := smc.createSecret(ctx, key, client)
+			if err != nil {
+				// Secret creation failed, bail
+				return err
+			}
+		} else {
+			// Some other error happened, lets just return
+			return err
+		}
+	}
+
+	err = smc.addSecretVersion(ctx, key, data, client)
+	if err != nil {
+		return err
+	}
+
+	smc.deleteOldSecretVersions(ctx, key, client, sv, svi)
+	return nil
+}
+
+// deleteOldSecretVersions will delete sv and all other SecretVersions within the svi.
+// This is a best effort operation and will not return any errors if there are problems,
+// but will log any problems (if debug logging is enabled).
+func (smc *GSMCache) deleteOldSecretVersions(
+	ctx context.Context,
+	key string, client *secretmanager.Client,
+	sv *secretmanagerpb.SecretVersion,
+	svi *secretmanager.SecretVersionIterator) {
+
+	for {
+		if sv == nil {
+			return
+		}
+
+		// This code will only ever leave them in the "ENABLED" state,
+		// so just try to delete those.
+		if sv.GetState() == secretmanagerpb.SecretVersion_ENABLED {
+			svr := &secretmanagerpb.DestroySecretVersionRequest{
+				Name: sv.GetName(),
+			}
+			_, err := client.DestroySecretVersion(ctx, svr)
+			if err != nil {
+				dlog("error deleting secret version: %v, got error %v", sv.GetName(), err)
+				return
+			}
+			dlog("Deleted secret %v", sv.GetName())
+		}
+		// Get the next SecretVersion to delete
+		var err error
+		sv, err = svi.Next()
+		if err != nil {
+			return
+		}
+	}
+}
+
+// createSecret will create the secret within the project.
+func (smc *GSMCache) createSecret(ctx context.Context, key string, client *secretmanager.Client) error {
+	createSecretReq := &secretmanagerpb.CreateSecretRequest{
+		Parent:   fmt.Sprintf("projects/%s", smc.ProjectId),
+		SecretId: fmt.Sprintf("%s%s", smc.SecretPrefix, key),
+		Secret: &secretmanagerpb.Secret{
+			Replication: &secretmanagerpb.Replication{
+				Replication: &secretmanagerpb.Replication_Automatic_{
+					Automatic: &secretmanagerpb.Replication_Automatic{},
+				},
+			},
+		},
+	}
+
+	_, err := client.CreateSecret(ctx, createSecretReq)
+	if err != nil {
+		return fmt.Errorf("Failed to create Secret. %w", err)
+	}
+	return nil
+}
+
+// addSecretVersion will store the data within the secret
+func (smc *GSMCache) addSecretVersion(ctx context.Context, key string, data []byte, client *secretmanager.Client) error {
+	sKey := fmt.Sprintf("projects/%s/secrets/%s%s", smc.ProjectId, smc.SecretPrefix, key)
+
+	req := &secretmanagerpb.AddSecretVersionRequest{
+		Parent: sKey,
+		Payload: &secretmanagerpb.SecretPayload{
+			Data: data,
+		},
+	}
+
+	_, err := client.AddSecretVersion(ctx, req)
+	return err
+}
+
+// Delete removes a certificate data from the cache under the specified key.
+// If there's no such key in the cache, Delete returns nil.
+func (smc *GSMCache) Delete(ctx context.Context, key string) error {
+	key = sanitize(key)
+
+	dlog("Delete called for: [%v]", key)
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		return fmt.Errorf("Failed to setup client: %w", err)
+	}
+
+	sKey := fmt.Sprintf("projects/%s/secrets/%s%s", smc.ProjectId, smc.SecretPrefix, key)
+
+	req := &secretmanagerpb.DeleteSecretRequest{
+		Name: sKey,
+	}
+
+	err = client.DeleteSecret(ctx, req)
+	if st := status.Convert(err); st != nil {
+		// No-such-key, we return nil
+		if st.Code() == codes.NotFound {
+			return nil
+		}
+		// Some other problem happened while trying to delete, return the error
+		return fmt.Errorf("Problem while deleting secret [%v]. %w", sKey, err)
+	}
+
+	return nil
 }
 
 func dlog(format string, v ...interface{}) {
 	if debug {
 		log.Printf(format, v...)
 	}
+}
+
+// Secret Manager URLs are a bit picky about the characters that can be in them.
+// This regex restricts the chars in the key passed in by autocert.
+// NOTE: any changes to this regex will be a breaking change.
+var allowedCharacters = regexp.MustCompile("[^a-zA-Z0-9-_]+")
+
+// Replace any non-URL safe characters with underscores.
+func sanitize(s string) string {
+	return allowedCharacters.ReplaceAllString(s, "_")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/jwendel/gsmcache
 
 go 1.13
+
+require (
+	cloud.google.com/go v0.52.0
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
+	google.golang.org/genproto v0.0.0-20200128133413-58ce757ed39b
+	google.golang.org/grpc v1.27.0
+)
